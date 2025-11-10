bac7108ed647df0d42029404c0de68e94cdb49cf
Reynard <reynardgeovanni@gmail.com>
2025-11-10 21:10:06 +0700

    Commit No. 1 – Building Jetpack Compose UI Commit No. 2 – State and Event Handler

---
+++ app/src/main/java/com/example/lab_week_09/MainActivity.kt
@@ -0,0 +1,117 @@
+package com.example.lab_week_09
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.lab_week_09.ui.theme.LAB_WEEK_09Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            LAB_WEEK_09Theme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Home()
+                }
+            }
+        }
+    }
+}
+
+data class Student(
+    var name: String
+)
+
+@Composable
+fun Home() {
+    val listData = remember {
+        mutableStateListOf(
+            Student("Tanu"),
+            Student("Tina"),
+            Student("Tono")
+        )
+    }
+
+    var inputField by remember { mutableStateOf(Student("")) }
+
+    HomeContent(
+        listData,
+        inputField,
+        { input -> inputField = inputField.copy(name = input) },
+        {
+            if (inputField.name.isNotBlank()) {
+                listData.add(inputField)
+                inputField = Student("")
+            }
+        }
+    )
+}
+
+@Composable
+fun HomeContent(
+    listData: SnapshotStateList<Student>,
+    inputField: Student,
+    onInputValueChange: (String) -> Unit,
+    onButtonClick: () -> Unit
+) {
+    LazyColumn {
+        item {
+            Column(
+                modifier = Modifier
+                    .padding(16.dp)
+                    .fillMaxSize(),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(text = stringResource(id = R.string.enter_item))
+
+                TextField(
+                    value = inputField.name,
+                    keyboardOptions = KeyboardOptions(
+                        keyboardType = KeyboardType.Text
+                    ),
+                    onValueChange = {
+                        onInputValueChange(it)
+                    }
+                )
+
+                Button(onClick = {
+                    onButtonClick()
+                }) {
+                    Text(text = stringResource(id = R.string.button_click))
+                }
+            }
+        }
+
+        items(listData) { item ->
+            Column(
+                modifier = Modifier
+                    .padding(vertical = 4.dp)
+                    .fillMaxSize(),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(text = item.name)
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewHome() {
+    Home(listOf("Tanu", "Tina", "Tono"))
+}
